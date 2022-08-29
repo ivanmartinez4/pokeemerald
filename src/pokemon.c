d6dc34051a54c38ebf02abcc8e4f52255e5c3ea7
CyanSMP64 <jmleuschke64@yahoo.com>
2022-08-29 17:26:23 +1200

    brhuh

--- src/pokemon.c
+++ src/pokemon.c
@@ -6399,14 +6399,14 @@ u16 GetBattleBGM(void)
         case SPECIES_ARTICUNO:
         case SPECIES_ZAPDOS:
         case SPECIES_MOLTRES:
-        #ifdef POKEMON_EXPANSION
+        #if P_NEW_POKEMON == TRUE
         case SPECIES_ARTICUNO_GALARIAN:
         case SPECIES_ZAPDOS_GALARIAN:
         case SPECIES_MOLTRES_GALARIAN:
         #endif
             return MUS_RG_VS_LEGEND;
         case SPECIES_MEWTWO:
-        #ifdef POKEMON_EXPANSION
+        #if P_NEW_POKEMON == TRUE
         case SPECIES_MEWTWO_MEGA_X:
         case SPECIES_MEWTWO_MEGA_Y:
         #endif
@@ -6428,7 +6428,7 @@ u16 GetBattleBGM(void)
         case SPECIES_REGIROCK:
         case SPECIES_REGICE:
         case SPECIES_REGISTEEL:
-        #ifdef POKEMON_EXPANSION
+        #if P_NEW_POKEMON == TRUE
         case SPECIES_REGIGIGAS:
         case SPECIES_REGIELEKI:
         case SPECIES_REGIDRAGO:
@@ -6436,7 +6436,7 @@ u16 GetBattleBGM(void)
             return MUS_VS_REGI;
         case SPECIES_LATIAS:
         case SPECIES_LATIOS:
-        #ifdef POKEMON_EXPANSION
+        #if P_NEW_POKEMON == TRUE
         case SPECIES_LATIAS_MEGA:
         case SPECIES_LATIOS_MEGA:
         #endif
@@ -6444,7 +6444,7 @@ u16 GetBattleBGM(void)
         case SPECIES_GROUDON:
         case SPECIES_KYOGRE:
         case SPECIES_RAYQUAZA:
-        #ifdef POKEMON_EXPANSION
+        #if P_NEW_POKEMON == TRUE
         case SPECIES_RAYQUAZA_MEGA:
         case SPECIES_KYOGRE_PRIMAL:
         case SPECIES_GROUDON_PRIMAL:
@@ -6453,13 +6453,13 @@ u16 GetBattleBGM(void)
         case SPECIES_JIRACHI:
             return MUS_VS_WILD;
         case SPECIES_DEOXYS:
-        #ifdef POKEMON_EXPANSION
+        #if P_NEW_POKEMON == TRUE
         case SPECIES_DEOXYS_ATTACK:
         case SPECIES_DEOXYS_DEFENSE:
         case SPECIES_DEOXYS_SPEED:
         #endif
             return MUS_RG_VS_DEOXYS;
-        #ifdef POKEMON_EXPANSION
+        #if P_NEW_POKEMON == TRUE
         case SPECIES_UXIE:
         case SPECIES_MESPRIT:
         case SPECIES_AZELF:
